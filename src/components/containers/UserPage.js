18d66dae84f955d91a078924c69a3a0b8b617d47
igomes <igomes@rdisoftware.com>
2018-08-14 19:10:02 -0300

    First redux call

--- src/components/containers/UserPage.js
+++ src/components/containers/UserPage.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import UserForm from '../forms/UserForm';
+import * as userActions from '../../actions/userActions';
 
 const UserPage = props => {
   return (
-    <UserForm />
+    <UserForm
+      users={props.users}
+      userActions={props.userActionsWithDispatch}
+    />
   );
 };
 
-export default UserPage;
+UserPage.propTypes = {
+  users: PropTypes.object,
+  userActionsWithDispatch: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    userActionsWithDispatch: bindActionCreators(userActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserPage);
--- src/components/forms/UserForm.js
+++ src/components/forms/UserForm.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class UserForm extends React.Component {
+  componentDidMount() {
+    debugger;
+    this.props.userActions.usersLoading(true);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    debugger;
+  }
+
   render() {
     return (
       <div className="container">
